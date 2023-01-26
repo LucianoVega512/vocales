31579d465d72a728c400f7eb863ec4f86c6cd0d4
Lucho <lucianomartin2015nqn@gmail.com>
2023-01-26 05:15:01 -0300

    Responsive

--- main.js
+++ main.js
@@ -17,11 +17,11 @@ establecerPrincipal(document.getElementById('analizar'));
 function reducir(texto) {
 	let letras = texto.split("");		
 	let vocales = letras.reduce(analizar, {a:0, e:0, i:0, o:0, u:0});
-  document.getElementById('principal').innerHTML = '<div id="contenedor_resultado"><div class="letra"><p class="txt-valor">' + 
+  document.getElementById('principal').innerHTML = '<div id="contenedor_resultado"><div class="letra superior"><p class="txt-valor">' + 
   	vocales.a + '</p><p class="txt-letra">Letter A</p></div><div class="letra"><p class="txt-valor">' + vocales.e + '</p><p class="txt-letra">Letter E</p></div>' +
   	'<div class="letra"><p class="txt-valor">'+vocales.i+'</p><p class="txt-letra">Letter I</p></div><div class="letra"><p class="txt-valor">' + 
 	vocales.o+ '</p><p class="txt-letra">Letter O</p></div><div class="letra"><p class="txt-valor">'+vocales.u+'</p><p class="txt-letra">Letter U</p>' +        
-  	'</div><div class="letra"><p class="txt-valor">'+ texto.length +'</p><p class="txt-letra">Letter Total</p></div>';
+  	'</div><div class="letra inferior"><p class="txt-valor">'+ texto.length +'</p><p class="txt-letra">Letter Total</p></div>';
 }
 
 function analizar(elementos, elemento) {	
