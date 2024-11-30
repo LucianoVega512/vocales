2415b75eacbcc8c4f081892813b2ebfcda36d307
Lucho <lucianomartin2015nqn@gmail.com>
2024-11-30 17:40:43 -0300

    Reacomoda css

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -20,6 +20,8 @@ export class DashboardComponent {
 
   public doughnutChartLabels: string[] = ['A', 'E', 'I', 'O', 'U'];
 
+  public cards: { clase: string, titulo: string }[] = [{ clase: 'grid-area: ca', titulo: 'VOCAL A' }, { clase: 'grid-area: ce', titulo: 'VOCAL E' }, { clase: 'grid-area: ci', titulo: 'VOCAL I' }, { clase: 'grid-area: co', titulo: 'VOCAL O' }, { clase: 'grid-area: cu', titulo: 'VOCAL U' }];
+
   public doughnutChartDatasets: ChartConfiguration<'doughnut'>['data']['datasets'] = [
     { data: [] }
   ];
@@ -28,10 +30,14 @@ export class DashboardComponent {
     responsive: true
   };
 
-  private vocales:Vocales;
+  public vocales: Vocales;
 
-  constructor(private router: Router){
+  constructor(private router: Router) {
     this.vocales = <Vocales>router.getCurrentNavigation()?.extras.state;
-    this.doughnutChartDatasets[0].data = this.vocales.data;    
+    this.doughnutChartDatasets[0].data = this.vocales.data;
+  }
+
+  public totalVocales(): number {
+    return this.vocales.data.reduce((a, b) => a + b);
   }
 }
