fd6980d8a7ac89180216a4d6917d9e7c36546623
Lucho <lucianomartin2015nqn@gmail.com>
2025-03-10 07:21:32 -0300

    agrega withHashLocation()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(withFetch()), provideCharts(withDefaultRegisterables())]
+  providers: [provideRouter(routes, withHashLocation()), provideHttpClient(withFetch()), provideCharts(withDefaultRegisterables())]
 };
